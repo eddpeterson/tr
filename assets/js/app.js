e77232bc9f12219641c267813ee74afb11145698
Edijs Petersons <eddpeterson@gmail.com>
2017-09-18 00:18:07 +0300

    Added interactive response to what user types in

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,5 +20,40 @@ import "phoenix_html"
 
 // import socket from "./socket"
 document.addEventListener("DOMContentLoaded",function(){
-  document.getElementById("type_in_text").focus()
+  document.getElementById("type_in_text").focus();
+  document.getElementById("type_in_text").onchange=changeEventHandler;
+  var el = document.getElementById("type_in_text");
+  el.addEventListener("keyup", changeEventHandler);
+  // document.querySelector('select[name="ice-cream"]').onchange=changeEventHandler;
+
+  var txt = [
+    "ho ho ho",
+    "ho ho ho",
+    "oh oh oh",
+    "oh oh oh",
+  ];
+  var status = [false, false, false, false];
+  var current = 0;
+
+  function changeEventHandler(event) {
+    if (event.key === "Enter") {
+      if (this.value === txt[current]) {
+        status[current] = true;
+        var q = `[data-index='${current}']`;
+        var el = document.querySelector(q);
+        el.className = "text-to-type";
+        
+        current = current + 1;
+        this.value = "";
+        this.placeholder = "";
+        if (current < txt.length) {
+          this.placeholder = `Type next: ${txt[current]}`;
+          var q = `[data-index='${current}']`;
+          var el = document.querySelector(q);
+          el.className = "current text-to-type";
+        }
+      }
+    }
+  }
+
 });
